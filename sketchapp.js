d792363e271256e87a293fccb836cd9972f07b29
magarini <56253774+magarini@users.noreply.github.com>
2021-09-26 23:50:10 +0300

    Add files via upload

---
+++ sketchapp.js
@@ -0,0 +1,76 @@
+var x = 0;
+var f = 0; // fill
+let slider;
+
+var song;
+var button;
+var jumpButton;
+
+function preload(){
+    song = loadSound('ola-mazi2.mp3', loaded);
+}
+
+
+function setup() {
+
+  select('#playbutton').mousePressed(togglePlaying);
+
+  canvas=createCanvas(windowWidth,windowHeight)
+//   slider = createSlider(0, width, 60, 10);
+//   slider.position(width/2-100, 3*height/4);
+//   slider.style('width', '200px');
+//   slider.input(update);
+  
+//   jumpButton.mousePressed(jumpSong);
+  
+}
+
+function draw() {
+  // let val = slider.value();
+  // background(val, 100, 100, 1);
+  background(20);
+  for (i=1; i<21;i++){
+    stroke(200);
+    line(i*width/20,height/10,i*width/20,9*height/10) 
+  }
+  push();
+  strokeWeight(4);
+stroke('red');
+  line(mouseX,height/10-10,mouseX, 9*height/10+10);
+  pop();
+}
+
+// function update() {
+//   	// get the value of the slider
+//   	var value = slider.value();
+//   	// the x position is set to value
+// 	x = value;
+//   	/* the fill color is mapped 
+//   		from slider range 0 - width		
+//   		to the range 0 - 255 */
+//   	f = map(value, 0, width, 0, windowWidth);
+// }
+
+function mouseMoved() {
+    var len = song.duration();
+    var t = map(mouseX, 0, width, 0, len);
+    
+    console.log(t);
+    song.jump(t);
+  }
+
+function togglePlaying() {
+    if (!song.isPlaying()) {
+      song.play();
+      song.setVolume(0.3);
+      select('#playbutton').html('pause');
+    } else {
+      song.stop();
+      select('#playbutton').html('play');
+    }
+  }
+  
+
+function loaded() {
+    console.log('loaded');
+  }
